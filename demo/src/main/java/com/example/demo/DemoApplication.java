66f79bc27f3245fe7c7bb3b3a065a6057d24c15c
Sanjeev Ghosh <sanjeev.ghosh@genpact.com>
2019-11-29 14:38:51 +0530

    Multi module demo project

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.util.*;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println(Constants.MODULE_NAME);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
