ac68f0293d240c3650d06578105fca009e7baa54
Sanjeev G <mailsanjeevg@gmail.com>
2020-01-25 14:06:30 +0530

    Added a RES ENd point /ping

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.util.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Depends on : "+Constants.MODULE_NAME);
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public String ping(){
+		return "Service is up";
 	}
 
 }
